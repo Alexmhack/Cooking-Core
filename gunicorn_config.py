aa21ab1a76b4411e5bc7a70929184b9f241cd025
Pranav <pranavg456@gmail.com>
2024-03-20 16:37:58 +0530

    make necessary changes for current projecct

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 """Gunicorn *development* config file"""
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
-wsgi_app = "project.wsgi:application"
+wsgi_app = "backend.wsgi:application"
 # The granularity of Error log outputs
 loglevel = "debug"
 # The number of worker processes for handling requests
@@ -11,10 +11,10 @@ bind = "0.0.0.0:8000"
 # Restart workers when code changes (development only!)
 reload = True
 # Write access and error info to /var/log
-accesslog = errorlog = "/var/log/gunicorn/dev.log"
+accesslog = errorlog = "/home/ubuntu/log/gunicorn/dev.log"
 # Redirect stdout/stderr to log file
 capture_output = True
 # PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/dev.pid"
+pidfile = "/home/ubuntu/log/gunicorn/dev.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = True
